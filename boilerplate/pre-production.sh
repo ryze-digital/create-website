cb3a598f0c7810b526cd5bf7bd40c10e57f0cd67
Christopher Canisius <cc@electronic-minds.de>
2024-12-03 15:32:28 +0100

    feat: Improve pre-production.sh failure handling
    Also uses newest version of Grunt since it is still compatible with
    grunt-cache-bust@1.7 and grunt-contrib-copy@1.0.

--- boilerplate/pre-production.sh
+++ boilerplate/pre-production.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-npm install grunt@1.3 grunt-cache-bust@1.7 grunt-contrib-copy@1.0 --no-package-lock
+set -euo pipefail
+
+npm install grunt@1.6 grunt-cache-bust@1.7 grunt-contrib-copy@1.0 --no-package-lock
 npm run pre-production
