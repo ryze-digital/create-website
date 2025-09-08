c6296787cb2c8b10cdb503e431b28e0ba58f1bb9
Christian Koop <contact@sprax2013.de>
2025-09-08 16:54:07 +0200

    feat(configurable-boilerplate): Display a nicer message when user Ctrl+Cs
    We can catch the error and only display the error message
    instead of showing the whole stack trace etc. that
    nobody is interested in.
    Any other error we might catch that way is re-thrown
    making it an unhandled error again.
    
    #6

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
 #!/usr/bin/env node
 
-import chalk from 'chalk';
+import chalk, { chalkStderr } from 'chalk';
 import fs from 'node:fs';
 import path from 'node:path';
 import { BoilerplateInstaller } from './src/BoilerplateInstaller.js';
 import { CliArgumentParser } from './src/CliArgumentParser.js';
+import { ExitPromptError } from '@inquirer/core';
 import { InteractiveUserInputs } from './src/InteractiveUserInputs.js';
 
-const parsedArgs = new CliArgumentParser().parseArgs();
-const targetInstallDir = process.cwd();
+/**
+ * @returns {Promise<void>}
+ */
+async function main() {
+    const parsedArgs = new CliArgumentParser().parseArgs();
+    const targetInstallDir = process.cwd();
 
-const interactiveUserInputs = new InteractiveUserInputs(parsedArgs.defaultProjectName, parsedArgs.defaultOutputPath);
-const installerResponses = await interactiveUserInputs.askAllInstallerQuestions();
+    const interactiveUserInputs = new InteractiveUserInputs(parsedArgs.defaultProjectName, parsedArgs.defaultOutputPath);
+    const installerResponses = await interactiveUserInputs.askAllInstallerQuestions();
 
-if (fs.existsSync(path.join(targetInstallDir, 'package.json'))) {
-    const shouldContinue = await interactiveUserInputs.askForConfirmation('A package.json file already exists. Continue?');
+    if (fs.existsSync(path.join(targetInstallDir, 'package.json'))) {
+        const shouldContinue = await interactiveUserInputs.askForConfirmation('A package.json file already exists. Continue?');
 
-    if (!shouldContinue) {
-        console.log(chalk.red('Aborting installation'));
-        process.exit(0);
+        if (!shouldContinue) {
+            console.log(chalk.red('Aborting installation'));
+            process.exit(0);
+        }
     }
+
+    await new BoilerplateInstaller().install(targetInstallDir, {
+        ...installerResponses,
+        logLevel: parsedArgs.logLevel,
+    });
+    console.info(chalk.green('Adventure ready'));
 }
 
-await new BoilerplateInstaller().install(targetInstallDir, { ...installerResponses, logLevel: parsedArgs.logLevel });
-console.info(chalk.green('Adventure ready'));
+try {
+    await main();
+} catch (err) {
+    if (err instanceof ExitPromptError) {
+        console.error(chalkStderr.red(err.message));
+        process.exit(1);
+    }
+
+    throw err;
+}
