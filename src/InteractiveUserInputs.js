28dc60921434f78efdeded269a9bef4618f71d64
Christian Koop <contact@sprax2013.de>
2025-09-08 16:18:29 +0200

    fix(configurable-boilerplate): Validate provided project name again
    This mimics the behavior before I introduced the interactivity.
    I keep using fallback project name as default as an example to the user.
    
    #6

--- src/InteractiveUserInputs.js
+++ src/InteractiveUserInputs.js
@@ -1,8 +1,10 @@
+import { confirm, input, select } from '@inquirer/prompts';
 import fs from 'node:fs';
 import { BOILERPLATE_PATH } from './constants.js';
-import { confirm, input, select } from '@inquirer/prompts';
 
 class InteractiveUserInputs {
+    static #FALLBACK_PROJECT_NAME = '@namespace/project-name';
+
     /**
      * @typedef {object} InstallerUserResponses
      * @property {string} projectName
@@ -14,7 +16,7 @@ class InteractiveUserInputs {
      * @param {string} defaultProjectName
      * @param {string} defaultOutputPath
      */
-    constructor(defaultProjectName = '@namespace/project-name', defaultOutputPath = 'build') {
+    constructor(defaultProjectName = InteractiveUserInputs.#FALLBACK_PROJECT_NAME, defaultOutputPath = 'build') {
         this.defaultProjectName = defaultProjectName;
         this.defaultOutputPath = defaultOutputPath;
     }
@@ -47,7 +49,17 @@ class InteractiveUserInputs {
      * @returns {Promise<string>}
      */
     #askProjectName() {
-        return input({ message: 'What should the project name be?', default: this.defaultProjectName });
+        return input({
+            message: 'What should the project name be?',
+            default: this.defaultProjectName,
+            validate: (value) => {
+                if (value === InteractiveUserInputs.#FALLBACK_PROJECT_NAME) {
+                    return 'Please enter a proper project name';
+                }
+
+                return true;
+            },
+        });
     }
 
     /**
